78bb2e31752babcd57cbd69d7f925f16c5de3e9e
Fish Nat <studyestudiante@gmail.com>
2024-07-22 19:04:58 -0400

    resultado redondeado

--- Desafio_1/escape.py
+++ Desafio_1/escape.py
@@ -4,4 +4,5 @@ radio=float(input("Ingrese el radio en Kilómetros:\n>"))
 
 # radio * 1000 es para hacer la conversión de km a m
 v_escape= math.sqrt(2*radio*1000*gravedad)
-print(f'La velocidad de escape es:{v_escape}')
+redondeado=round(v_escape,1)
+print(f'La velocidad de escape es:\n{redondeado}[m/s]')
