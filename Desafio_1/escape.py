44031a5027206e310b3bcb320e15c4f9e8926a6b
Fish Nat <studyestudiante@gmail.com>
2024-07-22 18:50:29 -0400

    calculo básico listo

---
+++ Desafio_1/escape.py
@@ -0,0 +1,7 @@
+import math
+gravedad= float(input("Ingrese la constante g:\n>"))
+radio=float(input("Ingrese el radio en Kilómetros:\n>"))
+
+# radio * 1000 es para hacer la conversión de km a m
+v_escape= math.sqrt(2*radio*1000*gravedad)
+print(f'La velocidad de escape es:{v_escape}')
